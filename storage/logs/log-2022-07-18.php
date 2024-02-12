a1ce45b63b0cd1a088c0881b3e21bec922084e95
Matias Zarate <151646244+matiaszarate0115@users.noreply.github.com>
2024-02-12 09:34:03 -0300

    Add files via upload

---
+++ storage/logs/log-2022-07-18.php
@@ -0,0 +1,57 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-07-18 04:15:11 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 04:15:11 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 05:11:56 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 05:11:56 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 05:12:46 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 05:12:46 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 05:14:05 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 05:14:05 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 05:48:43 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 05:48:49 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 4096 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1094
+ERROR - 2022-07-18 05:48:52 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 4096 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1094
+ERROR - 2022-07-18 05:49:04 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 05:49:07 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 4096 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1094
+ERROR - 2022-07-18 05:50:08 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 05:50:21 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 05:51:20 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 05:52:01 --> Severity: Error --> Allowed memory size of 134217728 bytes exhausted (tried to allocate 33554440 bytes) C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php 1093
+ERROR - 2022-07-18 06:05:14 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 06:05:14 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 06:18:23 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 06:18:23 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
+ERROR - 2022-07-18 06:29:17 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:20 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:32 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:40 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:46 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:47 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:49 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:29:50 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:30:47 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:30:48 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:31:59 --> Severity: error --> Exception: syntax error, unexpected '$config' (T_VARIABLE) C:\wamp64\www\ArasuFinal2\application\config\email.php 11
+ERROR - 2022-07-18 06:32:35 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2022-07-18 06:32:35 --> #0 C:\wamp64\www\ArasuFinal2\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\wamp64\www\ArasuFinal2\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\wamp64\www\ArasuFinal2\index.php(353): require_once('C:\\wamp64\\www\\A...')
+#3 {main}
